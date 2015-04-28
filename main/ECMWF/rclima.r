5f04ebdbc1e518a7ff90f56320487a8cc302059b
Alzira Ramos <z.gramos@gmail.com>
2015-04-28 18:28:23 +0100

    Update rclima.r

--- main/ECMWF/rclima.r
+++ main/ECMWF/rclima.r
@@ -1,6 +1,18 @@
+##list.files
+
+
 ## load the package
 require("zoo")
 require("rgdal")
 require("sp")
 require("matrixStats")
+
+####console setting
+###
+options(max.print=99999999) 
+options("scipen"=100, "digits"=4)
+
+# step 1#
+######import data ecmwf###
+#
 
