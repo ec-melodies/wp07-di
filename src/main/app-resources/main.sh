360cfbdc4377962bca2c26facd266d2d23e6e715
Alzira Ramos <zgramos@users.noreply.github.com>
2015-08-06 18:47:32 +0100

    update

--- src/main/app-resources/main.sh
+++ src/main/app-resources/main.sh
@@ -1,9 +1,9 @@
 chmod u+x /application/bin/main.py
 python /application/bin/main.py
 
+bash /application/bin/climatic_dataset_000.sh 
 bash /application/bin/climatic_dataset_001.sh 
 bash /application/bin/climatic_dataset_002.sh 
-bash /application/bin/climatic_dataset_003.sh 
 
 bash /application/bin/vgt_to_geoms_001.sh
 bash /application/bin/vgt_to_geoms_002.sh
