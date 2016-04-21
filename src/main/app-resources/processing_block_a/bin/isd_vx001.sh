60e260989f63011f8a18dfc3fcc5a3f727c1f608
Alzira Ramos <z.gramos@gmail.com>
2016-04-21 18:41:43 +0100

    update - geoserver

--- src/main/app-resources/processing_block_a/bin/isd_vx001.sh
+++ src/main/app-resources/processing_block_a/bin/isd_vx001.sh
@@ -19,24 +19,27 @@ export -p OUTDIR=$DIR/ISD000
 export -p ZDIR=$OUTDIR/GEOMS
 export -p ISDC=$ZDIR/Cx
 export -p ISDD=$ZDIR/Dx
+
+export -p isd_style=$ZDIR/isd_style.sld
 #-------------------------------------------------------------------------------------# 
 AOI="$( ciop-getparam aoi )"
 ciop-log "INFO" "AOI: $AOI"
 #-------------------------------------------------------------------------------------# 
-D=$(date +"%d%m%Y")
+export -p D=$(date +"%d%m%Y")
 
-for file in $ISDC/ISD_Cx002MSCAOI*.tif; do 
-filename=$(basename $file .tif )
-isd01=$ISDC/${filename}.tif
-isd02=$ISDD/${filename/#ISD_Cx002MSCAOI/ISD_Dx002MSCAOI}.tif 
-gdal_calc.py -A $isd01 -B $isd02 --outfile=$ZDIR/ISD_${D}_$AOI.tif --calc="((0.5*A)+(0.5*B))*10000" --NoDataValue=0 --overwrite  --type=UInt32
+# for file in $ISDC/ISD_Cx002MSCAOI*.tif; do 
+# filename=$(basename $file .tif )
+# isd01=$ISDC/${filename}.tif
+# isd02=$ISDD/${filename/#ISD_Cx002MSCAOI/ISD_Dx002MSCAOI}.tif 
+# gdal_calc.py -A $isd01 -B $isd02 --outfile=$ZDIR/ISD_${D}_$AOI.tif --calc="((0.5*A)+(0.5*B))*10000" --NoDataValue=0 --overwrite  --type=UInt32
 
-export -p INISD=$ZDIR/ISD_${D}_$AOI
-# publish the result
+# export -p INISD=$ZDIR/ISD_${D}_$AOI
+export -p INISD2=$ZDIR/ISD_${D}_$AOI.tif
+# # publish the result
 
-ciop-publish -m $ZDIR/ISD_${D}_$AOI.tif
-# s3cmd put --recursive $ZDIR/ISD_${D}_$AOI.tif s3://melodies-wp7/
-done
+# ciop-publish -m $ZDIR/ISD_${D}_$AOI.tif
+# # s3cmd put --recursive $ZDIR/ISD_${D}_$AOI.tif s3://melodies-wp7/
+# done
 #-------------------------------------------------------------------------------------# 
 #-------------------------------------------------------------------------------------# 
 #-------------------------------------------------------------------------------------# 
@@ -78,11 +81,10 @@ isd_style=`cat <<EOF
 EOF`
 
 echo $isd_style > $ZDIR/isd_style.sld
-echo $isd_style
 
 #-------------------------------------------------------------------------------------# 
 #-------------------------------------------------------------------------------------# 
-# Em construção...
+# Em construcao...
 # Upload_to_geoserver
 #-------------------------------------------------------------------------------------# 
 # Credenciais para geoserver
@@ -93,16 +95,33 @@ export -p username_geoserver='melodies-wp7'
 #EOF`
 #echo $variables_geoserver > $ZDIR/variables_geoserver.txt
 #-------------------------------------------------------------------------------------# 
-# cat <<EOF | /opt/anaconda/bin/python - 
-# import os
-# import sys
-# from sys import argv
-# import cioppy
-# ciop = cioppy.Cioppy()
-# sys.path.append('ZDIR')
-# tdir=os.environ['ZDIR']
-# target001=os.path.join(tdir,'ISD_${D}_$AOI.tif') 
-# EOF
+cat <<EOF | /opt/anaconda/bin/python - 
+import os
+import sys
+from sys import argv
+import subprocess
+import cioppy
+ciop = cioppy.Cioppy()
+
+sys.path.append('ZDIR')
+D=int(os.environ['D'])
+tdir=os.environ['ZDIR']
+file = os.environ['INISD2']
+
+# get the geoserver access point
+username = os.environ['username_geoserver']
+passw = 'changeme'
+host = os.environ['host_geoserver']
+workspace = os.environ['workspace_geoserver']
+#img.name = sub(".tif", "", basename(ISD)) 
+gfile = os.environ['isd_style']
+jobid = '2010'
+
+subprocess.call("curl -v -u \""+username+":"+passw+"\" -XPUT -H \"Content-type:image/tiff\" --data-binary @"+file+" "+host+"/workspaces/"+workspace+"/coveragestores/"+jobid+"--"+gfile+"/file.geotiff", shell=True)
+subprocess.call("curl -v -u \""+username+":"+passw+"\" -XPUT -H \"Content-type:application/xml\" -d \"<coverage><title>"+gfile[:-9]+"</title><enabled>true</enabled><advertised>true</advertised></coverage>\" "+host+"/workspaces/"+workspace+"/coveragestores/"+jobid+"--"+gfile+"/coverages/"+jobid+"--"+gfile[:-9]+".xml", shell=True)
+subprocess.call("curl -v -u \""+username+":"+passw+"\" -XPUT -H \"Content-type:application/xml\" -d \"<layer><defaultStyle><name>"+workspace+":"+style+"</name></defaultStyle><enabled>false</enabled><styles><style><name>raster</name></style><style><name>raster</name></style></styles><advertised>true</advertised></layer>\" "+host+"/layers/"+jobid+"--"+gfile[:-9]+".xml", shell=True)
+
+EOF
 #-------------------------------------------------------------------------------------#
 #-------------------------------------------------------------------------------------#
 #-------------------------------------------------------------------------------------#
