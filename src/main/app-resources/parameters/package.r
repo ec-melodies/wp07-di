d7ef9f2e8257c44c24136d9a1884a8ce6cbdfc86
Alzira Ramos <z.gramos@gmail.com>
2017-01-26 23:50:08 +0000

    update #122016
    update #122016

--- src/main/app-resources/parameters/package.r
+++
@@ -1,4 +0,0 @@
-xlist <- c("raster", "sp", "zoo", "rciop", "gtools", "digest", "rgdal")
-new.packages <- xlist[!(xlist %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-lapply(xlist, require, character.only = TRUE)
